3cb90285f5e75bc9af9a2d91ddbca824b8c816e7
sean <sean.snow@live.com>
2022-06-30 13:13:20 +0800

    feat: 导出模块

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './services/BasicProfileService.js';
+export * from './services/DataKey.js';
+export * from './services/DIDService.js';
+export * from './services/JOSEService.js';
+export * from './services/VCService.js';
+
+export * from './utils/DateUtil.js';
