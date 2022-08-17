0332f0666b4410e077073f08fa92e50e06bcae9a
sean <sean.snow@live.com>
2022-08-17 10:19:31 +0800

    feat: upgrade

--- test/BasicProfileService.test.ts
+++ test/BasicProfileService.test.ts
@@ -1,22 +1,20 @@
 import { BasicProfileService } from '../src/services/BasicProfileService.js';
 import { DIDService } from '../src/services/DIDService.js';
+import { ceramicApi } from './DIDService.test';
+import { mnemonicToSeed } from '@oneverse/utils';
 
 // 本地
-const id =
-  'did:3:kjzl6cwe1jw145psu2c1a3hl81a0705910euvjjziyw5k0mkvx6lmbd4yy8rd5d';
 const mnemonic =
   'check citizen casino bargain stamp noise quarter chunk toe dish raise debris execute sleep recipe buddy erase laundry record lobster repeat park bird phone';
 
-const account2 = {
-  mnemonic:
-    'undo chimney engage hurdle envelope suffer swarm world melody flavor parade clump game scrap property purse broom lava record similar velvet sleep dutch ocean',
-  did: 'did:3:kjzl6cwe1jw149xfxjomprlt3y56db86jw58kuje6hclw01bu11p8mbc884rzx6',
-};
+const authSecret: Uint8Array = mnemonicToSeed(mnemonic, { password: '' });
 
-const service = new DIDService();
+const service = await DIDService.newInstance({
+  authSecret,
+  ceramicApi,
+});
 
 async function test() {
-  await service.login(id, mnemonic);
   const basicProfileService = new BasicProfileService(service);
 
   const result = await basicProfileService.getProfile();
--- test/DIDService.test.ts
+++ test/DIDService.test.ts
@@ -3,13 +3,12 @@ import { mnemonicToSeed, randomMnemonic } from '@oneverse/utils';
 
 // const mnemonic =
 //   'arrest govern damp index ability speak sick caution hero tooth manual amazing trophy grant control flame inherit mother gate banana media digital nuclear advance';
-const password = '';
 
 const mnemonicRandom = randomMnemonic();
-// const password = '';
+const password = '123';
 
 export const authSecret: Uint8Array = mnemonicToSeed(mnemonicRandom, { password });
-export const ceramicApi = 'http://192.168.91.231:7007';
+export const ceramicApi = 'http://192.168.7.14:7007';
 
 async function login() {
   const didService = await DIDService.newInstance({
